e5d935e528bfcb5b43f460517096990ca46c38fb
virt <iv.chud@yandex.ru>
2023-10-17 11:29:04 +0000

    delete_db

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,4 +1,4 @@
-from config.loader import bot, db_user
+from config.loader import bot
 from config.state_menu import MenuStates
 from keyboards import menu_inline_kb
 from config.logger import logger
@@ -12,9 +12,6 @@ def send_welcome(message):
     :return:
     """
     logger.info(' ')
-    if not db_user.check_user(message.from_user.id):
-        db_user.add_user(message.from_user.id)
-        logger.error(f'user_id: {message.from_user.id}')
     bot.set_state(message.from_user.id, MenuStates.start, message.chat.id)
     bot.send_message(message.chat.id, 'Выберите раздел',
                      reply_markup=menu_inline_kb.create_buttons_federal_menu(**{'action': False,
