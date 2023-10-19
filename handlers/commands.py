16e417ac43f5fda952bcfd292b7032c71e1b4d02
virtuozlo <iv.chud@yandex.ru>
2023-10-19 15:05:10 +0300

    replace msg on function_msg

--- handlers/commands.py
+++ handlers/commands.py
@@ -2,18 +2,21 @@ from config.loader import bot
 from config.state_menu import MenuStates
 from keyboards import menu_inline_kb
 from config.logger import logger
+from utils.reader_files import get_msg
+from telebot.types import Message
 
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def send_welcome(message: Message):
     """
     Отправляет пользователя к выбору раздела
     :param message:
     :return:
     """
+    message, user_id, chat_id, message_id = get_msg(message)
     logger.info(' ')
     bot.set_state(message.from_user.id, MenuStates.start, message.chat.id)
-    bot.send_message(message.chat.id, 'Выберите раздел',
+    bot.send_message(chat_id, 'Выберите раздел',
                      reply_markup=menu_inline_kb.create_buttons_federal_menu(**{'action': False,
                                                                                 'part': False,
                                                                                 'article': False}))
@@ -21,4 +24,5 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['help'])
 def send_keyboard(message):
-    bot.send_message(message.chat.id, 'Бот тестируется и дополняется')
+    message, user_id, chat_id, message_id = get_msg(message)
+    bot.send_message(chat_id, 'Бот тестируется и дополняется')
--- handlers/menu_handlers.py
+++ handlers/menu_handlers.py
@@ -1,17 +1,19 @@
 from config.loader import bot
 from keyboards.kb_filters import *
 from keyboards.menu_inline_kb import create_buttons_federal_menu
-from utils.reader_files import file_reader
+from utils.reader_files import file_reader, get_msg
 from config.logger import logger
+from telebot.types import CallbackQuery
 
 
 @bot.callback_query_handler(func=None, start_config=for_start.filter())
-def go_prev(call):
+def go_prev(call: CallbackQuery):
     """
     Функция прорабатывает calldata и решает, какой параметр сделать false, что бы уйти на шаг назад
     :param call:
     :return:
     """
+    message, user_id, chat_id, message_id = get_msg(call, True)
     logger.info(' ')
     data = for_start.parse(callback_data=call.data)
     text = 'Выберете документ'
@@ -26,54 +28,57 @@ def go_prev(call):
     elif tmp.count(False) == 1:  # Добавить шаг назад (изменить часть на false)
         data['part'] = False
         text = 'Выберете главу'
-    bot.send_message(call.from_user.id, text, reply_markup=create_buttons_federal_menu(**{'action': data['action'],
-                                                                                          'part': data['part'],
-                                                                                          'article': data[
-                                                                                              'article']}))
-    bot.delete_message(call.message.chat.id, call.message.id)
+    bot.send_message(chat_id, text, reply_markup=create_buttons_federal_menu(**{'action': data['action'],
+                                                                                'part': data['part'],
+                                                                                'article': data[
+                                                                                    'article']}))
+    bot.delete_message(chat_id, message_id)
 
 
 @bot.callback_query_handler(func=None, start_config=for_menu_action.filter())
-def start_federal_menu(call):
+def start_federal_menu(call: CallbackQuery):
     """
     Ловит выбор пользователя из стартового меня.
     :param call: Выбор - федеральный кодекс
     :return: Перевод на меня выбора глав
     """
+    message, user_id, chat_id, message_id = get_msg(call, True)
     logger.info(' ')
     action = for_menu_action.parse(call.data)
-    bot.send_message(call.message.chat.id, f'Выберете главу',
+    bot.send_message(chat_id, f'Выберете главу',
                      reply_markup=create_buttons_federal_menu(**{'action': action['action'],
                                                                  'part': False,
                                                                  'article': False}))
-    bot.delete_message(call.message.chat.id, call.message.id)
+    bot.delete_message(chat_id, message_id)
 
 
 @bot.callback_query_handler(func=None, check_config=for_menu_part.filter())
-def get_federal_rules_part(call):
+def get_federal_rules_part(call: CallbackQuery):
     """
     Ловит выбор главы ФЗ
     :param call: Выбор - глава ФЗ
     :return:
     """
+    message, user_id, chat_id, message_id = get_msg(call, True)
     logger.info(' ')
     callback_data = for_menu_part.parse(callback_data=call.data)
-    bot.send_message(call.message.chat.id, f'Выберете Статью',
+    bot.send_message(chat_id, f'Выберете Статью',
                      reply_markup=create_buttons_federal_menu(**{'action': callback_data['action'],
                                                                  'part': callback_data['part'],
                                                                  'article': False
                                                                  }))
-    bot.delete_message(call.message.chat.id, call.message.id)
+    bot.delete_message(chat_id, message_id)
 
 
 @bot.callback_query_handler(func=None,
                             check_config=for_menu_article.filter())
-def get_some_article(call):
+def get_some_article(call: CallbackQuery):
     """
     Вывести статью
     :param call: Все интересующие данные пути до статьи
     :return:
     """
+    message, user_id, chat_id, message_id = get_msg(call, True)
     logger.info(' ')
     callback_data = for_menu_article.parse(callback_data=call.data)
     action, part, article = callback_data['action'], callback_data['part'], callback_data['article']
@@ -85,10 +90,10 @@ def get_some_article(call):
     finally:
         if not answer:
             answer = 'Файл пока пуст. Жди и посмотри что-то другое'
-        bot.send_message(call.message.chat.id, answer)
-        bot.send_message(call.message.chat.id, f'Выберете Статью',
+        bot.send_message(chat_id, answer)
+        bot.send_message(chat_id, f'Выберете Статью',
                          reply_markup=create_buttons_federal_menu(**{'action': action,
                                                                      'part': part,
                                                                      'article': False
                                                                      }))
-    bot.delete_message(call.message.chat.id, call.message.id)
+    bot.delete_message(chat_id, message_id)
